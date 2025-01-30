e770207adf64a7368673ccdc208d046d8eb6f9c4
Chris Mutchler <chris@virtualelephant.com>
2025-01-30 08:48:00 -0700

    portainer PS script

---
+++ create-portainer-environment.ps1
@@ -0,0 +1,42 @@
+# Assumes you have a working VCF environment and a VM template to clone
+
+$vcServer = "vcenter-vcf01.home.virtualelephant.com"
+$vcUsername = "administrator@vsphere.local"
+$vcPassword = ""
+
+Connect-VIServer -Server $vcServer -User $vcUsername -Password $vcPassword
+# Variables
+$templateName = "centos-stream-dhcp"
+$destinationFolder = "Portainer"
+$datastore = "ve-m01-cluster-001-vsan"  # Replace with your datastore
+$cluster = "ve-m01-cluster-001"      # Replace with your cluster
+$networkAdapterName = "Network adapter 1"
+$newPortGroup = "ve-portainer-1"
+
+$template = Get-VM -Name $templateName -ErrorAction Stop
+$cluster = Get-Cluster -Name $clusterName -ErrorAction Stop
+Write-Host "VM Template: $template"
+Write-Host "Cluster: $cluster"
+
+
+# Clone the template multiple times
+1..3 | ForEach-Object {
+    $vmName = "portainer-cntlr-$($_)" # Define VM name dynamically
+    New-VM -Name $vmName -VM $template -Datastore $datastore -Location $destinationFolder -ResourcePool $cluster
+    $vm = Get-VM -Name $vmName
+    $networkAdapter = Get-NetworkAdapter -VM $vm -Name $networkAdapterName
+    Set-NetworkAdapter -NetworkAdapter $networkAdapter -NetworkName $newPortGroup -Confirm:$false
+    Write-Host "Cloned VM: $vmName"
+}
+
+1..3 | ForEach-Object {
+    $vmName = "portainer-worker-$($_)" # Define VM name dynamically
+    New-VM -Name $vmName -VM $template -Datastore $datastore -Location $destinationFolder -ResourcePool $cluster
+    $vm = Get-VM -Name $vmName
+    $networkAdapter = Get-NetworkAdapter -VM $vm -Name $networkAdapterName
+    Set-NetworkAdapter -NetworkAdapter $networkAdapter -NetworkName $newPortGroup -Confirm:$false
+    Write-Host "Cloned VM: $vmName"
+}
+
+# Disconnect from vCenter
+Disconnect-VIServer -Server $vcServer -Confirm:$false
